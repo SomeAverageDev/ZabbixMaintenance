5fcdb8aa8401b9b0dd8bd858113cff9d1a6e3b71
SAD <zoulou31@free.fr>
2022-02-14 21:11:15 +0100

    First commit
    first working version, needs to improve hashString function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ZabbixMaintenance
 Template and action to manage dynamic maintenance on Zabbix
+
---
+++ Test/zabbix.maintenance.test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# create
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=10;comment=test maintenance A test-host1 with 1 tag with contains value;host=test-host1;tag=tag1:contains1:2"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=20;comment=test maintenance B test-host2 with 2 tags;host=test-host2;tag=CAP_CATEGORY:OS;tag=tag2:contains2:2"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=7000;comment=test maintenance C test-host3 no tags;host=test-host3"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=30;comment=test maintenance D with group TEST_HGP and 1 tag equal;group=TEST_HGP;tag=CAP_CATEGORY:OS"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=30;comment=test maintenance E with group TEST1_HGP no tags;group=TEST1_HGP"
+sleep 20
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;comment=test maintenance F with no duration;host=test-host2"
+sleep 20
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;host=test-host3"
+sleep 20
+
+#delete
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host1;tag=tag1:contains1:2"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host2;tag=CAP_CATEGORY:OS;tag=tag2:contains2:2"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;group=TEST_HGP;tag=CAP_CATEGORY:OS"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;group=TEST1_HGP"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host3"

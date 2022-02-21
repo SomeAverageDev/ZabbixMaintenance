43dce57f5806257e3698c26123c2ea77bbe677ab
SAD <zoulou31@free.fr>
2022-02-21 20:45:44 +0100

    update

--- Test/zabbix.maintenance.test.sh
+++ Test/zabbix.maintenance.test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # create
-zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=10;comment=test maintenance A test-host1 with 1 tag with contains value;host=test-host1;tag=tag1:contains1:2"
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=10;comment=test maintenance A test-host1 with 1 tag with contains value;host=test-host1;tag=tag1:equals1"
 sleep 3
-zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=20;comment=test maintenance B test-host2 with 2 tags;host=test-host2;tag=CAP_CATEGORY:OS;tag=tag2:contains2:2"
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=20;comment=test maintenance B test-host2 with 2 tags and no data collection;host=test-host2;tag=CAP_CATEGORY:OS;tag=tag2:contains2:2;nodata"
 sleep 3
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=7000;comment=test maintenance C test-host3 no tags;host=test-host3"
 sleep 3
@@ -11,13 +11,11 @@ zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMain
 sleep 3
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;duration=30;comment=test maintenance E with group TEST1_HGP no tags;group=TEST1_HGP"
 sleep 20
-zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;comment=test maintenance F with no duration;host=test-host2"
-sleep 20
-zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;host=test-host3"
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=create;comment=test maintenance F with no duration and no data collection;nodata;host=test-host2"
 sleep 20
 
 #delete
-zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host1;tag=tag1:contains1:2"
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host1;tag=tag1:equals1"
 sleep 3
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host2;tag=CAP_CATEGORY:OS;tag=tag2:contains2:2"
 sleep 3
@@ -26,3 +24,5 @@ sleep 3
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;group=TEST1_HGP"
 sleep 3
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host3"
+sleep 3
+zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s "Zabbix server" -k ZabbixMaintenance -o "action=delete;host=test-host2"
